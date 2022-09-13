e596e116d64080ceb91da616b5be76dcb3d9ef22
RayDev1988 <raymonddowden1988@gmail.com>
2022-09-13 11:39:30 -0700

    Environment install

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,21 +29,3 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, null)(Header);
-
-
-/*
-*                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home
-                                <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-* */
